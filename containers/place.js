65dbd638692ce449f6325b0fe9a4a0b665a07d2d
cygnauh <cygnauh@gmail.com>
2018-05-07 19:47:07 +0200

    load place

--- containers/place.js
+++ containers/place.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Button, ActivityIndicator,FlatList } from 'react-native';
+import { View, Text, Button, ActivityIndicator,FlatList, Animated, StyleSheet, Easing} from 'react-native';
 import Logo from '../components/logo';
 import GlobalStyle from '../styles/mainStyle';
 import TimerMixin from 'react-timer-mixin';
@@ -10,27 +10,66 @@ export default class Place extends React.Component{
         super(props);
         this.state = {
             value: 1,
-            isLoading: true
+            isLoading: true,
+            opacity: new Animated.Value(0.7)
         };
 
         this.colors =[
-            '#3A7BD2',
-            '#FFFF8D',
-            '#000',
-            '#C62828',
-            '#673AB7',
-            '#3F51B5',
-            '#009688',
-            '#FF9800',
-            '#9C27B0',
-            '#4CAF50'
+
+            '#e0650b', //volvan
+            '#e3a34b', //désert
+            '#f4ddac', //plage
+            '#66892c', //jungle
+            '#004c34', //forêt
+            '#1a507d', //lac
+            '#6b9adb', //ciel
+            '#c6c1b8', //montagne
+            '#7d7d7d', //ville
+            '#4f4640', //grotte
         ]
 
         this.color="#aaa"
         // this.changeColor(Colors, this.color)
 
+        this.animatedValue = new Animated.Value(0);
+
 
     }
+
+    animateBackgroundColor = () =>
+    {
+        this.animatedValue.setValue(0);
+        Animated.timing(
+            this.animatedValue,
+            {
+                toValue: 1,
+                duration: 1500,
+                easing: Easing.linear
+            }
+        ).start((animation) => {
+
+            // this.animateBackgroundColor()
+            if (animation.finished) {
+                this.animateBackgroundColor()
+                // this.spin();
+            }});
+
+
+
+        let stopAnimation = function(){
+            Animated.timing(
+                this.animatedValue
+            ).stop();
+
+        };
+        stopAnimation = stopAnimation.bind(this);
+
+        setTimeout( ()=>{
+            stopAnimation();
+        }, 3000);
+    }
+
+
 
     changeColor(){
             // setTimeout(function(){
@@ -42,82 +81,88 @@ export default class Place extends React.Component{
     }
 
     componentDidMount(){
-        let url = 'https://testappfabulab.herokuapp.com/places'
-        return fetch((url,{
-            //  mode:"no-cors",
-            method: "GET",
-            headers: {
-                'Accept': 'application/json',
-                "Content-Type": "text/plain"
-            }
-        }))
-            .then(function(response){
-                return response.json();
-            })
-            .then(function(json){
-                    this.setState({
-                        isLoading: false,
-                        dataSource: json,
-                    }, function(){
+        this.animateBackgroundColor();
 
-                    });
-                return {
-                    city: json.name,
-                }
+        return fetch('https://testappfabulab.herokuapp.com/places')
+            .then((response) => response.json())
+            .then((responseJson) => {
+
+                console.log(responseJson)
+
+                var random = Math.floor(Math.random() * Math.floor(responseJson.length));
+
+
+
+                this.setState({
+                    isLoading: false,
+                    dataSource: responseJson,
+                    randomPlace:random,
+                    randomPlaceName:responseJson[random]
+                }, function(){
+
+                });
+
             })
-            .catch(function(error) {
-                console.log('There has been a problem with your fetch operation: ' + error.message);
-                // ADD THIS THROW error
-                throw error;
+            .catch((error) =>{
+                console.error(error);
             });
-        // return fetch('https://testappfabulab.herokuapp.com/places')
-        //     .then((response) => response.json())
-        //     .then((responseJson) => {
-        //     //
-        //     //     this.setState({
-        //     //         isLoading: false,
-        //     //         dataSource: responseJson,
-        //     //     }, function(){
-        //     //
-        //     //     });
-        //     //
-        //     })
-        //     // .catch((error) =>{
-        //     //     console.error(error);
-        //     // });
     }
+
 
 
 
     render() {
 
-        // if(this.state.isLoading){
-        //     return(
-        //         <View style={{flex: 1, padding: 20}}>
-        //             <ActivityIndicator/>
-        //         </View>
-        //     )
-        // }
+        const backgroundColorVar = this.animatedValue.interpolate(
+            {
+                inputRange:[ 0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 1 ],
+                outputRange:  this.colors
+            });
+
+        if(this.state.isLoading){
+            return(
+                <View style={{flex: 1, padding: 20}}>
+                    <ActivityIndicator/>
+                </View>
+            )
+        }
         return(
-            <View style={[GlobalStyle.view, this.changeColor()]}>
+            <Animated.View style={[GlobalStyle.view, { backgroundColor: backgroundColorVar }]}>
                 <Logo/>
+
+                {/*<Animated.View style = {[ styles.container, { backgroundColor: backgroundColorVar } ]}>*/}
+                    {/*<Text style = { styles.text }>Animated </Text>*/}
+                {/*</Animated.View>*/}
+
                 <View>
-                    <Text style={GlobalStyle.titleContent}>Cette histoire se passe ...</Text>
+                    <Text style={GlobalStyle.titleContent}>Cette histoire se passe </Text>
 
                     <View style={GlobalStyle.placeContainer}>
-                        <Text>Cette histoire se passe ...</Text>
-                        <View style={{flex: 1, paddingTop:20}}>
-                            <FlatList
-                                data={this.state.dataSource}
-                                renderItem={({item}) => <Text>{item.title}, {item.releaseYear}</Text>}
-                                keyExtractor={(item, index) => index}
-                            />
-                        </View>
-
+                        {/*<Text>{this.state.dataSource[this.state.randomPlace].name}</Text>*/}
+                        <Text>{this.state.randomPlaceName.name}</Text>
                     </View>
                 </View>
                 <Button title={'Continuer'.toUpperCase()} onPress={() => this.props.navigation.navigate('Form', {length: this.state.value} )} />
-            </View>
+            </Animated.View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    text: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
