800d4fdc61c7a1e284e00bff66dc6e3fcdfb764e
Christine Yini Huang <cygnauh@gmail.com>
2018-06-12 01:40:26 +0200

    last story display, fix bug button home

--- containers/redirection.js
+++ containers/redirection.js
@@ -9,7 +9,7 @@ import {
     Animated
 
 } from 'react-native';
-import { RNCamera } from 'react-native-camera';
+import {RNCamera} from 'react-native-camera';
 import Header from '../components/header';
 import OnBoarding from "./onboarding";
 import GlobalStyle from "../styles/mainStyle";
@@ -35,97 +35,131 @@ export default class Redirection extends React.Component {
         super(props);
         this.state = {
             isLoading: false,
-            index:1,
+            index: 1,
             opacity: new Animated.Value(0)
         }
     }
 
-    animationDisplayValidation(){
+    animationDisplayValidation() {
         Animated.timing(this.state.opacity, {
             toValue: 1,
-            duration:500,
-            useNativeDriver:true}).start()
+            duration: 500,
+            useNativeDriver: true
+        }).start()
     }
 
-    redirectionWriting(){
+    redirectionWriting() {
 
-        if(this.state.index > 1){
+        if (this.state.index > 1) {
             this.state.index = 1
 
             this.setState({
-                scanValid:true
+                scanValid: true
             })
 
 
             this.animationDisplayValidation()
 
-            setTimeout(()=>{
+            setTimeout(() => {
                 this.props.navigation.navigate('Onboarding')
             }, 1000)
 
 
-
-        }else{
+        } else {
             this.setState({
-                index: this.state.index+1
+                index: this.state.index + 1
             })
         }
     }
 
-    redirectionReading(){
-            this.state.index = 1
+    redirectionReading() {
+
+        console.log("okok")
+        this.state.index = 1
 
         //default story
-            let num = 1462
-            let request = 'https://testappfabulab.herokuapp.com/storysoundsforreading?story=' + num;
 
-            return fetch(request)
+        // let num = this.props.navigation.state.params.num
+        //                         console.log(num)
+        // let request = 'https://testappfabulab.herokuapp.com/storysoundsforreading?story=' + num;
+        //
+        //
 
-                .then((response) => {
 
-                    if (response.status === 500) {
-                        this.setState({
-                            story_not_found: 1,
-                            isLoading: false
+        fetch('https://testappfabulab.herokuapp.com/laststory')
+
+            .then((response) => {
+
+                if (response.status === 500) {
+                    this.setState({
+                        story_not_found: 1,
+                        isLoading: false
+                    })
+                } else {
+                    response.json()
+                        .then((responseJson) => {
+                            this.setState({
+                                responseLastStory: responseJson
+                            });
+
+                            let num = this.state.responseLastStory[0].id
+
+                            fetch('https://testappfabulab.herokuapp.com/storysoundsforreading?story=' + num)
+
+                                .then((response) => {
+
+                                    if (response.status === 500) {
+                                        console.log("444")
+                                        this.setState({
+                                            story_not_found: 1,
+                                            isLoading: false
+                                        })
+                                    } else {
+                                        response.json()
+                                            .then((responseJson) => {
+                                                if (responseJson && responseJson.length === 4) {
+                                                    this.setState({
+                                                        response: responseJson
+                                                    });
+                                                }
+
+                                                if (responseJson && this.state.response) {
+
+
+                                                    console.log("hello")
+
+                                                    this.setState({
+                                                        scanValid: true
+                                                    })
+
+                                                    this.animationDisplayValidation()
+
+                                                    setTimeout(() => {
+                                                        this.props.navigation.navigate('ReadingMode', {responseJson: this.state.response})
+                                                    }, 1000)
+
+                                                }
+                                            })
+                                    }
+                                })
+
+
                         })
-                    } else {
-                        response.json()
-                            .then((responseJson) => {
-                                if (responseJson && responseJson.length === 4) {
-                                    this.setState({
-                                        response:responseJson
-                                    });
-                                }
+                }
+            })
 
-                                if(responseJson && this.state.response){
 
-                                    this.setState({
-                                        scanValid:true
-                                    })
-
-                                    this.animationDisplayValidation()
-
-                                    setTimeout(()=>{
-                                        this.props.navigation.navigate('ReadingMode',{responseJson: this.state.response})
-                                    },1000)
-
-                                }
-                                // this.setState({
-                                //     isLoading: false,
-                                // });
-                            })
-                    }
-                })
-
-                .catch((error) => {
-                    console.error(error);
-                });
+            .catch((error) => {
+                console.error(error);
+            });
     }
 
     render() {
         return (
-            <TouchableHighlight style={{flex:1}} onPress={()=>this.redirectionWriting()} onLongPress={()=>this.redirectionReading()}>
-                <View style={[GlobalStyle.view,styles.container]}>
+            <TouchableHighlight style={{flex: 1}}
+                                onPress={() => this.redirectionWriting()}
+                                onLongPress={() => this.redirectionReading()}>
+                <View style={[GlobalStyle.view, styles.container]}>
                     <Header
                         leftElm="back" rightElm="about"
                         onPress={() => this.props.navigation.goBack()}
@@ -136,15 +170,20 @@ export default class Redirection extends React.Component {
                         permissionDialogTitle={'Permission to use camera'}
                         permissionDialogMessage={'We need your permission to use your camera phone'}
                     >
-                        {({ camera, status }) => {
-                            if (status !== 'READY') return <PendingView />;
+                        {({camera, status}) => {
+                            if (status !== 'READY') return <PendingView/>;
                             return (
                                 <View>
-                                    <View style={{flex: 1,flexDirection: 'row', justifyContent: 'space-between', top:300}}>
+                                    <View style={{
+                                        flex: 1,
+                                        flexDirection: 'row',
+                                        justifyContent: 'space-between',
+                                        top: 300
+                                    }}>
                                         <Image style={{
                                             height: scaleWidth(150),
                                             width: scaleWidth(150),
-                                            paddingTop:20,
+                                            paddingTop: 20,
 
                                         }} source={require('../assets/images/scan/scan1.png')}/>
                                         {/*VALID*/}
@@ -155,8 +194,8 @@ export default class Redirection extends React.Component {
                                                 style={
                                                     {
                                                         position: "absolute",
-                                                        height: scaleWidth(20),
-                                                        width: scaleWidth(20),
+                                                        height: scaleWidth(16),
+                                                        width: scaleWidth(16),
                                                         left: (scaleWidth(150) / 2) - scaleWidth(20) / 2,
                                                         top: ((scaleWidth(150) / 2) - scaleWidth(20) / 2),
                                                         opacity: this.state.opacity,
@@ -170,12 +209,13 @@ export default class Redirection extends React.Component {
                                                 }
                                                 source={require('../assets/images/scan/scanOk.png')}
                                             />
-                                        ):null}
+                                        ) : null}
 
                                     </View>
-                                    <View style={{opacity: 0 ,flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-                                        <TouchableOpacity onPress={() => this.takePicture(camera)} style={styles.capture}>
-                                            <Text style={{ fontSize: 14 }}> SNAP </Text>
+                                    <View style={{opacity: 0, flex: 0, flexDirection: 'row', justifyContent: 'center'}}>
+                                        <TouchableOpacity onPress={() => this.takePicture(camera)}
+                                                          style={styles.capture}>
+                                            <Text style={{fontSize: 14}}> SNAP </Text>
                                         </TouchableOpacity>
                                     </View>
                                 </View>
@@ -184,13 +224,13 @@ export default class Redirection extends React.Component {
                         }}
                     </RNCamera>
                 </View>
-             </TouchableHighlight>
+            </TouchableHighlight>
 
         );
     }
 
-    takePicture = async function(camera) {
-        const options = { quality: 0.5, base64: true };
+    takePicture = async function (camera) {
+        const options = {quality: 0.5, base64: true};
         const data = await camera.takePictureAsync(options);
         //  eslint-disable-next-line
         console.log(data.uri);
